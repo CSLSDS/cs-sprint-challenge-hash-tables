639c827b019fb63e914fa245d32de657037b7943
cslsds <lambdadatasciencelearning@gmail.com>
2020-08-07 12:35:25 -0400

    ex1 passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ht = {}
+    for index in range(length):
+        requisite = limit - weights[index]
+        if requisite in ht: # second pass; ix will be the latter
+            return (index, ht[requisite])
+        else:
+            ht[weights[index]] = index
 
     return None
