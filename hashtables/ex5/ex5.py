df76d628d08c2b35971701c3ea8e4434fbf0ad84
cslsds <lambdadatasciencelearning@gmail.com>
2020-08-07 13:08:50 -0400

    ex5 passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,17 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ht = {}
+    
+    for f in files:
+        tail = f.rpartition('/')[2]
+        if tail not in ht:
+            ht[tail] = []
+        ht[tail].append(f)
+    result = []
+    for query in queries:
+        if query in ht:
+            result += ht[query]
 
     return result
 
@@ -23,3 +33,34 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+
+    files = []
+
+    for i in range(500000):
+        files.append(f"/dir{i}/file{i}")
+
+    for i in range(500000):
+        files.append(f"/dir{i}/dirb{i}/file{i}")
+
+    queries = []
+
+    for i in range(1000000):
+        queries.append(f"nofile{i}")
+
+    queries += [
+        "file3490",
+        "file256",
+        "file999999",
+        "file8192"
+    ]
+
+    result = finder(files, queries)
+    result.sort()
+
+    print(result)
+
+    print(['/dir256/dirb256/file256',
+        '/dir256/file256', '/dir3490/dirb3490/file3490',
+        '/dir3490/file3490', '/dir8192/dirb8192/file8192',
+        '/dir8192/file8192'])
